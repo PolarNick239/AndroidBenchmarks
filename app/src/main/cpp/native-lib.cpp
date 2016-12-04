2cb746556c0e5a05b34e85ff0255acab150b1661
PolarNick239 <PolarNick239@gmail.com>
2016-12-04 21:42:01 +0300

    Colors drawing now native (huge speedup)

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,30 @@
 #include <jni.h>
 #include <string>
+#include <android/bitmap.h>
+
+extern "C"
+void
+Java_com_polarnick_androidbenchmarks_life_LifeDrawerThread_draw(JNIEnv *env, jobject instance,
+                                                                jintArray statesJ,
+                                                                jintArray colorsPaletteJ,
+                                                                jobject img) {
+    jint* states = env->GetIntArrayElements(statesJ, JNI_FALSE);
+    jint* colorsPalette = env->GetIntArrayElements(colorsPaletteJ, JNI_FALSE);
+
+    jint* pixels;
+    AndroidBitmapInfo imgInfo ;
+
+    AndroidBitmap_lockPixels(env, img, (void **) &pixels);
+    AndroidBitmap_getInfo(env, img, &imgInfo);
+
+    for (int i = 0; i < imgInfo.width * imgInfo.height; ++i) {
+        pixels[i] = colorsPalette[states[i]];
+    }
+
+    AndroidBitmap_unlockPixels(env, img);
+    env->ReleaseIntArrayElements(statesJ, states, 0);
+    env->ReleaseIntArrayElements(colorsPaletteJ, colorsPalette, 0);
+}
 
 extern "C"
 void
--- app/src/main/java/com/polarnick/androidbenchmarks/life/LifeDrawerThread.java
+++ app/src/main/java/com/polarnick/androidbenchmarks/life/LifeDrawerThread.java
@@ -138,13 +138,6 @@ public class LifeDrawerThread implements Runnable {
         return (a << 24) | (r << 16) | (g << 8) | b;
     }
 
-    private void draw(int[] states, int[] colorsPalette, Bitmap img) {
-        for (int y = 0; y < img.getHeight(); ++y) {
-            for (int x = 0; x < img.getWidth(); ++x) {
-                int state = states[img.getWidth() * y + x];
-                img.setPixel(x, y, colorsPalette[state]);
-            }
-        }
-    }
+    private native void draw(int[] states, int[] colorsPalette, Bitmap img);
 
 }
